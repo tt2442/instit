9f1c196cb6c4226d8a3d7dff9d80f4349c2bac69
tt2442 <theokdo@gmail.com>
2021-05-09 17:04:31 +0200

    raz

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,121 +1,120 @@
 <?php
-
-namespace  App\Controller;
-
-use App\Entity\Article;
+//******no_regenerate*****
+namespace  App\Controller ;use App\Entity\Article;
 use App\Form\ArticleType;
-use App\Repository\ArticleRepository;
+    use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use DeepCopy\DeepCopy;
-use DeepCopy\Filter\SetNullFilter;
-use DeepCopy\Matcher\PropertyNameMatcher;
 
 /**
  * @Route("admin/article")
- */ class ArticleController extends AbstractController
+ */class ArticleController extends AbstractController
 {
     /**
-     * @Route("/", name="article_index", methods={"GET"})
-     */
+    * @Route("/", name="article_index", methods={"GET"})
+    */
     public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('article/index.html.twig', [
-            'article' => $articleRepository->findAll(),
-        ]);
+    return $this->render('article/index.html.twig', [
+    'articles' => $articleRepository->findAll(),
+    ]);
     }
 
     /**
-     * @Route("/new", name="article_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $article = new Article();
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($request);
+    * @Route("/new", name="article_new", methods={"GET","POST"})
+    */
+public function new(Request $request): Response
+{
+$article = new Article();
+$form = $this->createForm(ArticleType::class, $article);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($article);
-            $entityManager->flush();
+if ($form->isSubmitted() && $form->isValid()) {
+$entityManager = $this->getDoctrine()->getManager();
+$entityManager->persist($article);
+$entityManager->flush();
 
-            return $this->redirectToRoute('article_index');
-        }
+return $this->redirectToRoute('article_index');
+}
 
-        return $this->render('article/new.html.twig', [
-            'article' => $article,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="article_show", methods={"GET"})
-     */
-    public function show(Article $article): Response
-    {
-        return $this->render('article/show.html.twig', [
-            'article' => $article,
-        ]);
-    }
+return $this->render('article/new.html.twig', [
+'article' => $article,
+'form' => $form->createView(),
+]);
+}
 
     /**
-     * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Article $article): Response
-    {
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('article_index');
-        }
-
-        return $this->render('article/edit.html.twig', [
-            'article' => $article,
-            'form' => $form->createView(),
-        ]);
-    }
+    * @Route("/{id}", name="article_show", methods={"GET"})
+    */
+public function show(Article $article): Response
+{
+return $this->render('article/show.html.twig', [
+'article' => $article,
+]);
+}
 
     /**
-     * @Route("/{id}/copy", name="article_copy", methods={"GET","POST"})
-     */
-    public function copy(Request $request, Article $articlec): Response
-    {
-        $copier = new DeepCopy();
-        $copier->addFilter(new SetNullFilter(), new PropertyNameMatcher('id'));
-        $article = $copier->copy($articlec);
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($request);
+    * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
+    */
+public function edit(Request $request, Article $article): Response
+{
+$form = $this->createForm(ArticleType::class, $article);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($article);
-            $entityManager->flush();
+if ($form->isSubmitted() && $form->isValid()) {
+$this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('article_index');
-        }
+return $this->redirectToRoute('article_index');
+}
 
-        return $this->render('article/new.html.twig', [
-            'article' => $article,
-            'form' => $form->createView(),
-        ]);
-    }
+return $this->render('article/new.html.twig', [
+'article' => $article,
+'form' => $form->createView(),
+]);
+}
 
     /**
-     * @Route("/{id}", name="article_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Article $article): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($article);
-            $entityManager->flush();
-        }
+    * @Route("/{id}/copy", name="article_copy", methods={"GET","POST"})
+    */
+public function copy(Request $request, Article $articlec): Response
+{
+$article = clone $articlec;
 
-        return $this->redirectToRoute('article_index');
-    }
+$em = $this->getDoctrine()->getManager();
+$em->persist($article);
+$em->flush();
+return $this->redirectToRoute('article_index');
+//$article = $copier->copy($articlec);
+//$form = $this->createForm(ArticleType::class, $article);
+//$form->handleRequest($request);
+
+if ($form->isSubmitted() && $form->isValid()) {
+$entityManager = $this->getDoctrine()->getManager();
+$entityManager->persist($article);
+$entityManager->flush();
+
+return $this->redirectToRoute('article_index');
+}
+
+return $this->render('article/new.html.twig', [
+'article' => $article,
+'form' => $form->createView(),
+]);
+}
+
+    /**
+    * @Route("/{id}", name="article_delete", methods={"POST"})
+    */
+public function delete(Request $request, Article $article): Response
+{
+if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+$entityManager = $this->getDoctrine()->getManager();
+$entityManager->remove($article);
+$entityManager->flush();
+}
+
+return $this->redirectToRoute('article_index');
+}
 }
