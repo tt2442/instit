29a5f1cbb2e14d2fd5bf873f91783f2af0ce5047
tt2442 <theokdo@gmail.com>
2021-06-25 16:31:01 +0200

    page d'accueil fini sans hover

---
+++ src/Form/Categorie2Type.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Form;use App\Entity\Categorie ;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use App\Entity\Article;
+
+use App\Entity\Promotion;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+class Categorie2Type extends AbstractType
+{
+public function buildForm(FormBuilderInterface $builder, array $AtypeOption)
+{
+$builder->add('Nom', TextType::class,['attr'=>['data-inputmask'=>"'alias': 'texte_propre'",]])
+
+->add('Description', null,['attr'=>[]])
+->add('Etat', ChoiceType::class,['choices' =>['Actif'=>'true','Inactif'=>'false',],'attr'=>[]])
+->add('couleur', ColorType::class,['attr'=>['data-inputmask'=>"'alias': 'color'",]])
+->add('article', EntityType::class,[
+                'class' => Article::class,
+                'multiple' => true,'required'=>false,'attr'=>['no_index',]])
+->add('Promotion', EntityType::class,[
+                'class' => Promotion::class,
+                'multiple' => true,'required'=>false,'attr'=>['no_index',]])
+
+;}
+
+public function configureOptions(OptionsResolver $resolver)
+{
+$resolver->setDefaults([
+    'data_class' => Categorie::class,
+]);
+}
+}
